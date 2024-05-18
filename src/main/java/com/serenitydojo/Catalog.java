b8c02f0b537cd3a5d9ed621c281542cf51bcc488
pastrypossum <j.j@j.com>
2024-05-18 11:19:29 +0100

    Updated tests

--- src/main/java/com/serenitydojo/Catalog.java
+++ src/main/java/com/serenitydojo/Catalog.java
@@ -1,20 +1,64 @@
 package com.serenitydojo;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Catalog {
-    public void setPriceOf(Fruit fruit, double price) {
-        throw new RuntimeException("TODO, create a map to keep track of fruits and their prices");
-        // fruitToPrice.put(fruit.name(), price);
-    }
+
+    private static Map<Fruit, CatalogItem> currentStock = new HashMap<>();
 
     public static Catalog withItems(CatalogItem... catalogItems) {
-        throw new RuntimeException("TODO, create catalog and add items to the list of available fruits");
-        // Catalog catalog = new Catalog();
-        // for (CatalogItem catalogItem : catalogItems) {
-        //     catalog.availableFruits.add(catalogItem);
-        // }
-        // return catalog
+
+        Catalog catalog = new Catalog();
+        for(CatalogItem item: catalogItems){
+            currentStock.put(item.getFruit(), item);
+        }
+
+        return catalog;
     }
 
+    public void setPriceOf(Fruit fruit, double price) {
+
+        currentStock.get(fruit).setPricePerKg(price);
+    }
+
+    public Double getPriceOf(Fruit fruit) throws FruitUnavailableException {
+
+        if(!currentStock.containsKey(fruit))
+            throw new FruitUnavailableException("Fruit is not available");
+
+        return currentStock.get(fruit).getPricePerKg();
+    }
+
+    public void setQuantityOf(Fruit fruit, int quantity) {
+
+        currentStock.get(fruit).setQuantity(quantity);
+    }
+
+    public int getQuantityOf(Fruit fruit) throws FruitUnavailableException {
+
+        if(!currentStock.containsKey(fruit) || currentStock.get(fruit).getQuantity() == 0)
+            throw new FruitUnavailableException("Fruit is not available");
+        else
+            return currentStock.get(fruit).getQuantity();
+    }
+
+    public List<String> getAvailableFruits() {
+
+        return currentStock.values().stream()
+                .map(value -> value.getFruit().name())
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public Map<Fruit,Double> getPriceList() {
+
+        Map<Fruit,Double> result = new HashMap<>();
+
+        for(Fruit fruit: currentStock.keySet()){
+            result.put(fruit, currentStock.get(fruit).getPricePerKg());
+        }
+
+        return result;
+    }
 }
